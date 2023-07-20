16916684f53b336aacf88bd5ac869f4de4aa6937
Yashkura <102008549+Yashkura@users.noreply.github.com>
2023-07-20 15:32:25 +0530

    v 2.0

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => res.send("Yaswanth Backend  App v1.0"));
+app.get("/", (req, res) => res.send("Yaswanth Backend  App v2.0"));
 app.get("/users", function (req, res) {
   res.status(200).json([
     {
